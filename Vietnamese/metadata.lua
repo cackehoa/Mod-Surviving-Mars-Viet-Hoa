3580582ac4b41a73a0adca2e6a7788a953581b8d
Lệ Tà <89197762+cackehoa@users.noreply.github.com>
2021-09-15 12:38:08 +0700

    Tải lên 2 tập tin metadata.lua và Vietnamese.csv
    Tải 2 tập tin còn lại lên thư mục Vietnamese. Tiến trình dịch: dịch xong phần hướng dẫn cơ bản

---
+++ Vietnamese/metadata.lua
@@ -0,0 +1,19 @@
+return PlaceObj('ModDef', {
+	'title', "Tiếng Việt",
+	'description', "Mod nhằm thay đổi ngôn ngữ game thành tiếng Việt",
+	'last_changes', "Dịch hết phần hướng dẫn",
+	'id', "tAFx2up",
+	'pops_desktop_uuid', "a0a8dd7a-95bc-4ba1-aa1b-222b9a18d7ad",
+	'pops_any_uuid', "f2932f4e-d229-4222-81be-aa8465c441ad",
+	'author', "Cắc kè hoa",
+	'version', 9,
+	'lua_revision', 233360,
+	'saved_with_revision', 1001514,
+	'loctables', {
+		{
+			filename = "Vietnamese.csv",
+			language = "English",
+		},
+	},
+	'saved', 1631683392,
+})
